38133eb8d3478a44ac9d1155a3f568760638392a
dj-kadam95 <deshmukhdhananjay76@gmail.com>
2026-08-01 05:43:50 +0530

    31 july

--- Selenium_maven_using_POM/src/test/java/may_30_test_components/B_May_30_base_test.java
+++ Selenium_maven_using_POM/src/test/java/may_30_test_components/B_May_30_base_test.java
@@ -4,11 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -53,7 +53,7 @@ public class B_May_30_base_test{
 		File src =((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		File dest = new File(System.getProperty("user.dir")+"//reports//"+testCaseName+".png");
 
-			FileUtils.copyFile(src, dest);
+			FileHandler.copy(src, dest);
 		return System.getProperty("user.dir")+"//reports//"+testCaseName+".png";
 			
 		}
