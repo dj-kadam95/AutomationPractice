282c63d0b9e0cb244ae936079ed8798a552a5c14
dj-kadam95 <deshmukhdhananjay76@gmail.com>
2026-09-06 11:21:32 +0530

    medicart_proj

--- medicart_project/src/test/java/testCases_step3/Testcase3_search_product.java
+++ medicart_project/src/test/java/testCases_step3/Testcase3_search_product.java
@@ -6,7 +6,7 @@ import test_components.Base_class_step1;
 
 public class Testcase3_search_product extends Base_class_step1 {
 	
-	@Test
+	@Test(groups="regression")
 	public void searchProduct()
 	{
 		url.goToURL();
