0b21d64ad1645496d699ab044c95920cd3819ef2
dj-kadam95 <deshmukhdhananjay76@gmail.com>
2026-09-07 20:45:50 +0530

    medicart_proj

--- medicart_project/src/main/java/pom/D_Prod_to_card.java
+++ medicart_project/src/main/java/pom/D_Prod_to_card.java
@@ -3,8 +3,8 @@ package pom;
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.openqa.selenium.JavascriptExecutor;
-//import org.openqa.selenium.Point;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -37,14 +37,14 @@ public class D_Prod_to_card extends WebDriverwait {
 	public void selectProd() {
 		waitForElement(prod);
 
-//		Point p1 = prod.getLocation();
-//
-//		//int x = p1.getX();
-//		int y = p1.getY();
-//		int y1=y-200;
-//		JavascriptExecutor js = (JavascriptExecutor) driver;
-//
-//		js.executeScript("window.scrollBy(0," + y1 + ")");
+		Point p1 = prod.getLocation();
+
+		//int x = p1.getX();
+		int y = p1.getY();
+		int y1=y-200;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		js.executeScript("window.scrollBy(0," + y1 + ")");
 		prod.click();
 	}
 
