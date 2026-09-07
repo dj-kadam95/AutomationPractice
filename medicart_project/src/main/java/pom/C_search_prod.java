2003fff2f688453ee2af3030327cc4d3547bcd1b
dj-kadam95 <deshmukhdhananjay76@gmail.com>
2026-09-07 21:36:41 +0530

    medicart_proj

--- medicart_project/src/main/java/pom/C_search_prod.java
+++ medicart_project/src/main/java/pom/C_search_prod.java
@@ -23,7 +23,7 @@ public class C_search_prod extends WebDriverwait {
 
 	}
 
-	@FindBy(xpath = "//input[@class='t4s-search-header__input t4s-input__currentcolor']")
+	@FindBy(xpath = "//button[@class='t4s-search-header__submit']/preceding-sibling::*")
 	WebElement searchBox;
 
 	@FindBy(xpath = "//button[@class='t4s-search-header__submit']")
