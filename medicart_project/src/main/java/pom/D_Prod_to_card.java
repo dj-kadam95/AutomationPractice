8905417b453f4c96e6b010c24c12170835df9059
dj-kadam95 <deshmukhdhananjay76@gmail.com>
2026-09-07 20:47:47 +0530

    medicart_proj

--- medicart_project/src/main/java/pom/D_Prod_to_card.java
+++ medicart_project/src/main/java/pom/D_Prod_to_card.java
@@ -3,8 +3,8 @@ package pom;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Point;
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -23,10 +23,10 @@ public class D_Prod_to_card extends WebDriverwait {
 		PageFactory.initElements(driver, this);
 	}
 
-	@FindBy(xpath = "//div[@class='t4s-top-heading t4s_des_title_1']/following-sibling::div/descendant::span[contains(text(), 'CPAP Machine')]")
-	WebElement prod;
-//	@FindBy(xpath = "(//span[text()='CPAP Machine'])[2]")
+//	@FindBy(xpath = "//div[@class='t4s-top-heading t4s_des_title_1']/following-sibling::div/descendant::span[contains(text(), 'CPAP Machine')]")
 //	WebElement prod;
+	@FindBy(xpath = "(//span[text()='CPAP Machine'])[2]")
+	WebElement prod;
 
 	@FindBy(xpath = "//div[@class='t4s-product-wrapper']/descendant::a[@class='is--href-replaced']")
 	List<WebElement> products;
@@ -37,14 +37,14 @@ public class D_Prod_to_card extends WebDriverwait {
 	public void selectProd() {
 		waitForElement(prod);
 
-		Point p1 = prod.getLocation();
-
-		//int x = p1.getX();
-		int y = p1.getY();
-		int y1=y-200;
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-
-		js.executeScript("window.scrollBy(0," + y1 + ")");
+//		Point p1 = prod.getLocation();
+//
+//		//int x = p1.getX();
+//		int y = p1.getY();
+//		int y1=y-200;
+//		JavascriptExecutor js = (JavascriptExecutor) driver;
+//
+//		js.executeScript("window.scrollBy(0," + y1 + ")");
 		prod.click();
 	}
 
