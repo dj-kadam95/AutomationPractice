c493315b7f22f127179d59702a0d54d04a3761f2
dj-kadam95 <deshmukhdhananjay76@gmail.com>
2026-09-08 20:27:36 +0530

    Medivart_proj

--- medicart_project/src/main/java/pom/D_Prod_to_card.java
+++ medicart_project/src/main/java/pom/D_Prod_to_card.java
@@ -33,7 +33,7 @@ public class D_Prod_to_card extends WebDriverwait {
 	@FindBy(xpath = "//div[@class='t4s-product-wrapper']/descendant::a[@class='is--href-replaced']")
 	List<WebElement> products;
 
-	@FindBy(xpath = "//div[@class='t4s-product-wrapper']/descendant::span[contains(text(), 'Add to cart')]/parent::a")
+	@FindBy(xpath = "//div[@class='t4s-product-wrapper']//span[contains(normalize-space(), 'Add to cart')]/ancestor::*[self::button or self::a][1]")
 	List<WebElement> cartBtn;
 
 	@FindBy(xpath = "(//button[@class='t4s-drawer__close'])[1]")
